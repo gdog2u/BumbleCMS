6000ea67d7cf32c91490180c868f7c2bb50aab0b
Hudson <gdog2u@gmail.com>
2021-06-19 18:13:22 -0400

    Add code to get page template

--- index.php
+++ index.php
@@ -6,15 +6,16 @@
 	
 	include("functions.php");
 
-	getSlugTemplate("");
+	$slug = getSlugFromURI();
 
-	if(isset($_GET['p']) && is_numeric($_GET['p'])):
-		$post = Post::getByID($_GET['p']);
-?>
-	<pre><?=print_r($post, true) ?></pre>
-	<h1><?=$post->Title ?></h1>
-	<div class="summary"><?=$post->Summary ?></div>
-	<div class="body"><?=$post->Body ?></div>
-<?php
-	endif;
+	$template = getSlugTemplate($slug[0]);
+
+	if(file_exists(VIEWS_PATH.$template))
+	{
+		include(VIEWS_PATH.$template);
+	}
+	else
+	{
+		display404();
+	}
 ?>
