945a6ef5ae5bb69bd64b6f64a110d0678bd4002c
Hudson <gdog2u@gmail.com>
2021-06-19 18:13:04 -0400

    Add function to get slug, and display 404 page

--- functions.php
+++ functions.php
@@ -1,8 +1,22 @@
 <?php
 
+function getSlugFromURI(): array
+{
+    $uri = preg_split("/\//", $_SERVER["REQUEST_URI"], -1, PREG_SPLIT_NO_EMPTY);
+
+    return $uri;
+}
+
 function getSlugTemplate(string $slug): string
 {
     global $dbh;
    
     return "";
+}
+
+function display404()
+{
+    http_response_code(404);
+    include(VIEWS_PATH."errors/404.php");
+    exit(0);
 }
