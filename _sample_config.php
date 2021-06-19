160f366a6eb9a67cda3ae37eb7d78d01dea24110
Hudson <gdog2u@gmail.com>
2021-06-19 17:46:50 -0400

    Add new constant for template file path

--- _sample_config.php
+++ _sample_config.php
@@ -4,6 +4,7 @@ ini_set("dispaly_errors", true);
 /* Paths */
 define("CLASS_PATH", "classes/");
 define("UPLOADS_PATH", "uploads/");
+define("VIEWS_PATH", "views/");
 /* Database */
 define("DB_HOST", "localhost");
 define("DB_DSN", "mysql:host=".DB_HOST.";dbname=BumbleCMS");
