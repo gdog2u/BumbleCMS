0fcb918feb2343e1519676937ee29b203b506669
Hudson <gdog2u@gmail.com>
2022-04-05 20:40:42 -0400

    Add base post file

---
+++ themes/BumbleCMS/post.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * Post.php
+     * @author Bee Hudson
+     */
+    include_once('./functions.php');
+
+    include_once('./partials/header.php');
+?>
+    
+<?php
+    include_once('./partials/footer.php');
+?>
