1e81e09542c9240f22548b9371ab67c17e184cda
Hudson <gdog2u@gmail.com>
2022-04-08 11:39:52 -0400

    Add temporary output for posts

--- themes/BumbleCMS/post.php
+++ themes/BumbleCMS/post.php
@@ -9,10 +9,12 @@
     $p = Post::getBySlug($slug[1]);
     if(!$p){ display404(); }
     
-
     include_once('partials/header.php');
 ?>
-    
+<h1><?=$p->Title ?></h1>
+<div id="post-body">
+    <?=$p->Body ?>
+</div>
 <?php
     include_once('partials/footer.php');
 ?>
