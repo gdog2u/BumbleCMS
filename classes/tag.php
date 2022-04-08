0afdf8efa54de00632622c6e3ec4877f005c9aed
Hudson <gdog2u@gmail.com>
2022-04-08 13:28:47 -0400

    Add base Tag class

--- classes/tag.php
+++ classes/tag.php
@@ -3,4 +3,39 @@
 class Tag
 {
 
+	// Properties
+	public $TagID = null;
+	public $TagName = null;
+
+	public function __construct($data = array())
+	{
+		if(isset($data['TagID'])){ $this->TagID = $data["TagID"]; }
+
+		if(isset($data['TagName'])){ $this->TagName = $data["TagName"]; }
+	}
+
+	/* Get Functions */
+	public static function getByID(int $id)
+	{
+		if(!isset($id)){ return null; }
+
+        $conn = new PDO(DB_DSN, DB_USER, DB_PASS);
+        $get = $conn->prepare("
+            SELECT *
+            FROM PostTags
+            WHERE TagID=?
+        ");
+
+        if(!$get->execute([$id]))
+        {
+            return null;
+        }
+
+        $tag = $get->fetch();
+        $conn = null;
+
+        if($tag){ return new Tag($tag); }
+
+        return null;
+	}
 }
