06412778ba8888f3f9084cd64895f43b8cc4ec0f
Hudson <gdog2u@gmail.com>
2022-04-08 12:40:06 -0400

    Add printing of 3 latest posts

--- themes/BumbleCMS/home.php
+++ themes/BumbleCMS/home.php
@@ -10,6 +10,28 @@
 
 <h1>Welcome to my site!</h1>
 
+<div id="recent-posts">
+	<h1>Recent Posts:</h1>
+<?php
+	$posts = Post::getNRows(3);
+
+	if(!$posts['count']): ?>
+	<div class="post-preview">
+		<h2>No recent Posts</h2>
+	</div>
+<?php
+	else:
+		while($p = array_shift($posts['posts'])): ?>
+	<div class="post-preview">
+		<h2><a href="#"><?=$p->Title ?></a></h2>
+		<div class="post-summary"><?=$p->Summary ?></div>
+	</div>
+<?php
+		endwhile;
+	endif;
+?>
+</div>
+
 <?php
     include_once('partials/footer.php');
 ?>
